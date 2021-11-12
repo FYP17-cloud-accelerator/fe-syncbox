4b40dea62469dd7d102aecee6ed5060e413fa587
Melanga Kasun <melangakasun@gmail.com>
2021-11-12 14:19:56 +0530

    Schedule Download

--- src/dashboard/DashboardPage.js
+++ src/dashboard/DashboardPage.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { useContext, useEffect, useState } from "react";
-import { Accordion, AccordionContext, Card, Col, ListGroup, Row, useAccordionButton } from "react-bootstrap";
+import { Accordion, AccordionContext, Card, Col, Form, FormControl, InputGroup, ListGroup, Row, useAccordionButton } from "react-bootstrap";
 import { base_url } from "../App";
 import useToken from "../auth/Token";
 
 export default function DashboardPage() {
     const [fileData, setFileData] = useState({});
+    const [startDate, setStartDate] = useState();
+    const [startTime, setStartTime] = useState();
     const { token } = useToken();
     let location = decodeURI(window.location.pathname.substr(6));
     console.log(location);
@@ -21,6 +23,14 @@ export default function DashboardPage() {
         }
     }, []);
 
+    const handleSubmit = async (filename, e) => {
+        e.preventDefault();
+        console.log(startDate, startTime, filename);
+        if (startDate && startTime) {
+            scheduleDownload(token?.user, `${location}/${filename}`, startDate, startTime);
+        }
+    }
+
     return (
         <div>
             {fileData?.directories?.length > 0 || fileData?.files?.length > 0 ? (
@@ -30,7 +40,7 @@ export default function DashboardPage() {
                             return <ListGroup.Item key={key}>
                                 <Card>
                                     <Card.Body>
-                                        <a href={`/data/${location}/${directory.name}`}>{directory.name}</a> ({directory.size})
+                                        <a href={`/data/${location}/${directory.name}`}>{directory.name}</a>
                                     </Card.Body>
                                 </Card>
                             </ListGroup.Item>
@@ -50,8 +60,34 @@ export default function DashboardPage() {
                                         <Card.Footer>
                                             <ListGroup>
                                                 <ListGroup.Item action onClick={() => downloadFile(token?.user, `${location}/${file.name}`)}>Download Now</ListGroup.Item>
-                                                <ListGroup.Item action onClick={alertClicked}>Overnight Download</ListGroup.Item>
-                                                <ListGroup.Item action onClick={alertClicked}>Schedule Download</ListGroup.Item>
+                                                <ListGroup.Item action>
+                                                    <Row>
+                                                        <Col xs={6} md={4}>Schedule Download</Col>
+                                                        <Col xs={12} md={8}>
+                                                            <Form onSubmit={(e) => handleSubmit(file.name, e)}>
+                                                                <Row className="align-items-center">
+                                                                    <Col xs="auto">
+                                                                        <Form.Label htmlFor="inlineFormInput" visuallyHidden>Start Time</Form.Label>
+                                                                        <InputGroup className="mb-2">
+                                                                            <InputGroup.Text>DATE</InputGroup.Text>
+                                                                            <FormControl type="date" placeholder="Start Time" onChange={e => setStartDate(e.target.value)} />
+                                                                        </InputGroup>
+                                                                    </Col>
+                                                                    <Col xs="auto">
+                                                                        <Form.Label htmlFor="inlineFormInputGroup" visuallyHidden>End Time</Form.Label>
+                                                                        <InputGroup className="mb-2">
+                                                                            <InputGroup.Text>TIME</InputGroup.Text>
+                                                                            <FormControl type="time" placeholder="End Time" onChange={e => setStartTime(e.target.value)} />
+                                                                        </InputGroup>
+                                                                    </Col>
+                                                                    <Col xs="auto">
+                                                                        <button type="submit" className="btn mb-2 btn-sm btn-primary">Schedule</button>
+                                                                    </Col>
+                                                                </Row>
+                                                            </Form>
+                                                        </Col>
+                                                    </Row>
+                                                </ListGroup.Item>
                                             </ListGroup>
                                         </Card.Footer>
                                     </Accordion.Collapse>
@@ -81,8 +117,18 @@ async function downloadFile(username, filename) {
     }
 }
 
-function alertClicked() {
-    alert('Clicked !');
+async function scheduleDownload(username, filename, startDate, startTime) {
+    if (username && filename && startDate && startTime) {
+        return axios.get(`${base_url}/schedule?username=${username}&filename=${filename}&day=${startDate}&time=${startTime}`)
+            .then(response => {
+                console.log(response.data);
+                return response.data;
+            })
+            .catch(error => {
+                console.error(error);
+                return error;
+            });
+    }
 }
 
 function CustomToggle({ children, eventKey, callback }) {
